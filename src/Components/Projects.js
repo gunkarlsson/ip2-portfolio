cfb59d020fc566dc829a37b30b8db7a90d622e08
gunkarlsson <hi@gunkarlsson.com>
2021-02-01 17:07:48 +0100

    🚧  wip, github api

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,25 +1,6 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-
-// const Users = () => {
-//   const database = [
-//     {
-//       name: "Gun",
-//       age: "28",
-//       location: "Sthlm",
-//     },
-//     {
-//       name: "Gunnar",
-//       age: "38",
-//       location: "Nacka",
-//     },
-//     {
-//       name: "Gunnel",
-//       age: "48",
-//       location: "Eslöv",
-//     },
-//   ];
-// };
+import Repo from "./Components/Repo";
 
 const ProjectDiv = styled.div`
   /* min-height: 100vh; */
@@ -35,17 +16,48 @@ const ProjectDiv = styled.div`
 `;
 
 function Projects() {
+  const API_URL = "https://api.github.com/users/gunkarlsson/repos";
+  const [allRepos, setAllRepos] = useState([]);
+
+  // useEffect(() => {
+  //   async function fetchApi(API_URL) {
+  //     const res = await fetch(API_URL);
+  //     const data = await res.json();
+  //     setAllRepos(data);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((response) => response.json())
+      .then((data) => setAllRepos(data));
+  }, []);
+  console.log(allRepos);
+  allRepos.map(({ id, name }) => {
+    return allRepos;
+  });
+
   return (
     <ProjectDiv>
       <h1>Projects</h1>
       <h1 className="number">01</h1>
-      {/* {users.map((user) => {
-        {
-          user.name;
-        }
-      })} */}
+      <Repo />
     </ProjectDiv>
   );
 }
 
 export default Projects;
+
+// {
+//   movies.map(({ id, title, overview, vote_average, poster_path }) => {
+//     return (
+//       <MovieItem
+//         title={title}
+//         key={id}
+//         overview={overview}
+//         voteAverage={vote_average}
+//         posterPath={poster_path}
+//       />
+//     );
+//   });
+// }
---
+++ src/Components/Repo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Repo = () => {
+  return (
+    <div>
+      <p>HELLOOOOoooOOo</p>
+    </div>
+  );
+};
+
+export default Repo;
