a5591f0c362401c6a7453d9c83c98ed2e6e61171
gunkarlsson <hi@gunkarlsson.com>
2021-02-02 10:39:34 +0100

    🚧  working on projects-page

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import Repo from "./Components/Repo";
+import Repo from "./Repo";
 
 const ProjectDiv = styled.div`
   /* min-height: 100vh; */
@@ -11,13 +11,37 @@ const ProjectDiv = styled.div`
   }
 
   h1:last-of-type {
-    margin-left: 200px;
+    margin-left: 300px;
+  }
+
+  button {
+    outline: none;
+    border: none;
+    background-color: rgba(255, 255, 255, 0.3);
+    padding: 1rem 1.5rem;
+    cursor: pointer;
+  }
+
+  button:hover {
+    background-color: rgba(255, 255, 255, 0.6);
   }
 `;
 
 function Projects() {
   const API_URL = "https://api.github.com/users/gunkarlsson/repos";
   const [allRepos, setAllRepos] = useState([]);
+  const [repoIndex, setRepoIndex] = useState(0); //inom parantes är vad den börjar med
+
+  const decrement = () => {
+    if (repoIndex > 0) {
+      setRepoIndex((last) => last - 1);
+    }
+  };
+  const increment = () => {
+    if (repoIndex < allRepos.length - 1) {
+      setRepoIndex((last) => last + 1);
+    }
+  };
 
   // useEffect(() => {
   //   async function fetchApi(API_URL) {
@@ -32,16 +56,18 @@ function Projects() {
       .then((response) => response.json())
       .then((data) => setAllRepos(data));
   }, []);
-  console.log(allRepos);
-  allRepos.map(({ id, name }) => {
-    return allRepos;
-  });
 
   return (
     <ProjectDiv>
       <h1>Projects</h1>
-      <h1 className="number">01</h1>
-      <Repo />
+      {repoIndex < 9 ? <h1>0{repoIndex + 1}</h1> : <h1>{repoIndex + 1}</h1>}
+      {allRepos.length > 0 && <Repo repo={allRepos[repoIndex]} />}
+      <button onClick={decrement}>
+        <i class="fas fa-arrow-left"></i>
+      </button>
+      <button onClick={increment}>
+        <i class="fas fa-arrow-right"></i>
+      </button>
     </ProjectDiv>
   );
 }
--- src/Components/Repo.js
+++ src/Components/Repo.js
@@ -1,10 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-const Repo = () => {
+const RepoDiv = styled.div`
+  overflow: hidden;
+  top: 0;
+  right: 0;
+  width: 70vw;
+  height: 60vh;
+  border: 2px solid red;
+
+  p {
+    font-family: "Helvetica";
+  }
+
+  img {
+    width: 300px;
+  }
+`;
+
+const Repo = ({ repo }) => {
+  const COMMITS_URL =
+    "https://api.github.com/repos/gunkarlsson/ip2-portfolio/commits";
+  const [commits, setCommits] = useState([]);
+
+  useEffect(() => {
+    fetch(COMMITS_URL)
+      .then((response) => response.json())
+      .then((data) => setCommits(data));
+    console.log(commits);
+  }, []);
+
   return (
-    <div>
-      <p>HELLOOOOoooOOo</p>
-    </div>
+    <RepoDiv>
+      <p>
+        Project name: {repo.name.charAt(0).toUpperCase() + repo.name.slice(1)}
+      </p>
+      <p>Description: {repo.description}</p>
+      <p>
+        Github link: <a href="{repo.html_url}">{repo.name}</a>
+      </p>
+      <p>Created at: {repo.created_at}</p>
+      <p>Updated at: {repo.updated_at.slice(0, -10)}</p>
+      <img
+        src="https://images.unsplash.com/photo-1552865703-8168fcd8d541?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"
+        alt=""
+      />
+      {/* <p>`https://api.github.com/repos/gunkarlsson/${repo.name}/commits`</p> */}
+    </RepoDiv>
   );
 };
 
